e044d58d31ad3c2469e6ffb42dba576fc2b355e9
Albert Chen <chenalbert60@gmail.com>
2015-02-10 07:42:48 -0800

    yeahhhhhhhh  boiiiiiiiiiiiiiiiii viiiiiiiideoooooooooooooooooooooo 2999

--- model/config.php
+++ model/config.php
@@ -1,3 +1,9 @@
 <?php 
     $path = "/chena-blog/";
+    
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "blog_db";
+        
 
--- view/form.php
+++ view/form.php
@@ -1,6 +1,9 @@
+<?php
+require_once(__DIR__ . "/../model/config.php");
+?>  
 <h1>Create Blog Post</h1>
 
-<form>
+<form method="post" action="<?php echo $path . "controller/create-post.php";?>">
     <div>
         <label form="title">Title: </label>
         <input type="text" name="title" />
