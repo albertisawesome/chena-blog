e6345cc2a03ebc5769996f6a241883d77bb2d0de
Albert Chen <chenalbert60@gmail.com>
2015-02-18 15:03:22 -0800

    a;dskja;sdlfkjas;dflkjasd;lfkjasd;flksaj

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,7 +2,7 @@
 
 require_once (__DIR__ ."/../model/config.php");
 
-$query = $connection->query("CREATE TABLE posts ("
+$query = $_SESSION["connection"]->query("CREATE TABLE posts ("
         . "id int(11) NOT NULL AUTO_INCREMENT,"
         . "title varchar(255) NOT NULL,"
         . "post text NOT NULL,"
@@ -13,6 +13,20 @@ if($query){
     echo "<p>Succesfully created table:posts</p>";
 }
 else{
-    echo "<p>$connection->error</p>";
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
 }
 
+$query = $_SESSION["connection"]->query("CREATE TABLE users ("
+        . "id int(11) NOT NULL AUTO_INCREMENT, "
+        . "username varchar(30) NOT NULL,"
+        . "email varchar(50) NOT NULL,"
+        . "password char(128) NOT NULL,"
+        . "salt char(128) NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+if($query) {
+    echo "<p>Successfully created table: users</p>";
+}
+else {
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}
--- model/config.php
+++ model/config.php
@@ -1,11 +1,16 @@
 <?php
     require_once (__DIR__ . "/database.php");
-    
+    session_start();
     $path = "/chena-blog/";
     
     $host = "localhost";
     $username = "root";
     $password = "root";
     $database = "blog_db";
-        
-    $connection = new Database($host, $username, $password, $database);
+    
+    if(!isset($_SESSION["connection"])) {
+            
+  
+        $connection = new Database($host, $username, $password, $database);
+        $_SESSION["connection"] = $connection;
+    }
