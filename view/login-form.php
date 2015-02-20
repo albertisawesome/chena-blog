71d2d6486e61fa2381a8f9bfff24e4240eda4cd6
Albert Chen <chenalbert60@gmail.com>
2015-02-20 15:02:30 -0800

    yeash 446

--- view/login-form.php
+++ view/login-form.php
@@ -2,28 +2,22 @@
 require_once(__DIR__ . "/../model/config.php");
 ?>  
 
-<h1>Register</h1>
+<h1>Login</h1>
 
-<form method="post" action="<?php echo $path . "controller/create-user.php";?>">
-    <div>
-        <label for="email">Email: </label>
-        <input type="text" name="email" />
-    </div>
-    
+<form methodd="post" action="<?php echo $path . "controller/login-user.php"?>">
+
     <label for="username">Username: </label>
     <input type="text" name="username" />
     <div>
-    
+
     </div
     <label for="password">Password: </label>
     <input type="password" name="password" />
     <div>
-        
+
     </div>
-    
+
     <div>
         <button type="submit">Submit</button>
     </div>
-
 </form>
-
