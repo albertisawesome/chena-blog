391f333818641cf05626db5d30f99bc4e90846dd
Albert Chen <chenalbert60@gmail.com>
2015-02-25 14:59:30 -0800

    asdkjfhasdlfjhasdjkfh

--- view/login-form.php
+++ view/login-form.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . "/../model/config.php");
 
 <h1>Login</h1>
 
-<form methodd="post" action="<?php echo $path . "controller/login-user.php"?>">
+<form method="post" action="<?php echo $path . "controller/login-user.php"?>">
 
     <label for="username">Username: </label>
     <input type="text" name="username" />
